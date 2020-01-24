2db086e49304b375c75da1b2867184896c161394
Fred <fred@Freds-MacBook-Pro.local>
2020-01-24 11:51:37 -0500

    Adding logging messages in Spring 5.1

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -1,4 +1,11 @@
 package com.luv2code.springdemo;
 
 public class MyApp {
+    public static void main(String[] args) {
+        //create the object
+        Coach theCouch = new TrackCoach();
+
+        //use the object
+        System.out.println(theCouch.getDailyWorkout());
+    }
 }
--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -1,4 +1,9 @@
 package com.luv2code.springdemo;
 
-public class TrackCoach {
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5K";
+    }
 }
