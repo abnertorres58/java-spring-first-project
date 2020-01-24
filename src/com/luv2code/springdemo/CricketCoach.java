5bfcf4685d74d08b49fc6ca575cf631f83ececfa
Fred <fred@Freds-MacBook-Pro.local>
2020-01-24 12:49:03 -0500

    adding a new coach in order to test IoC with XML configuration

---
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your aim for at least an hour a day";
+    }
+}
