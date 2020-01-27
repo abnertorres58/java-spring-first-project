21a75925497a152bf0e2d2c4b1ae4f14794b157d
Fred <fred@Freds-MacBook-Pro.local>
2020-01-27 11:52:18 -0500

    Setter injection

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -5,15 +5,25 @@ public class CricketCoach implements Coach {
     //define a private field for dependency
     private FortuneService fortuneService;
 
+    //generate no argument constructor
+    public CricketCoach() {
+        System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
     //define a constructor for dependency injection
     public CricketCoach (FortuneService theFortuneService){
         fortuneService = theFortuneService;
     }
 
+    //our setter method
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("CricketCoach: inside setter method - setFortuneService");
+        this.fortuneService = fortuneService;
+    }
 
     @Override
     public String getDailyWorkout() {
-        return "Practice your aim for at least an hour a day";
+        return "Practice fast bowling for 15 minutes";
     }
 
     @Override
