014fd2f112005c008e8b0c01b9a49614027587d5
Fred <fred@Freds-MacBook-Pro.local>
2020-01-29 14:40:02 -0500

    Some extra test bean Scope and life cycle  with XML configuration

--- src/com/luv2code/springdemo/BeanLifeCycleDemoApp.java
+++ src/com/luv2code/springdemo/BeanLifeCycleDemoApp.java
@@ -13,6 +13,19 @@ public class BeanLifeCycleDemoApp {
         //Retrieve the bean from Spring container
         Coach theCoach = context.getBean("myCoach",Coach.class);
 
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+
+        //Check if they are the same
+        boolean result = theCoach == alphaCoach;
+
+        //print out the results
+        System.out.println("\nPointing to the same object: " + result);
+
+        System.out.println("\nMemory location for theCoach: " + theCoach);
+
+        System.out.println("\nMemory location for alphaCoach: " + alphaCoach + "\n");
+
         System.out.println(theCoach.getDailyWorkout());
         //Close context
         context.close();
